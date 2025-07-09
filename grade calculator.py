22804de5f58bb9f428cbf0732a3a4f93759d8dde
sathi000000007 <sathi7019756776@gmail.com>
2025-07-09 21:35:36 +0530

    Create grade calculator.py

---
+++ grade calculator.py
@@ -0,0 +1,9 @@
+num=int(input(" enter a num 0-100 "))
+if num>=90 and num<=100:
+    print( " grade a ")
+elif num>=75 and num<=89:
+    print(" grade B")
+elif num>=50 and num<=74:
+    print(" grade c")
+else:
+    print("fail")
